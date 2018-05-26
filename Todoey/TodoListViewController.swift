2df917e9f91b2ed67a6ee6042f4b436855751fbb
Mike Soertsz <mike@hammock-studio.com>
2018-05-26 15:48:04 +0530

    Array items saved and loaded from userdefaults pList file.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,13 +10,16 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    var itemArray = ["Find Mike", "Buy Eggos", "Kill Demagorgon"]
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
-
-    
-    var itemArray = ["Find Mike", "Buy Eggos", "Kill Demagorgon"]
     
     //MARK - Tableview data source methods
     
@@ -57,6 +60,7 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what happens when the user clicks the Add Item button in the UIAlert popup dialog.
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey:"TodoListArray")
             self.tableView.reloadData()
         }
         
